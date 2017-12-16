e940f8714f838463b1a6ef5195c1d8fa2c648e9e
Lasana Murray <lmurray@quenk.com>
2017-12-16 10:53:23 -0400

    refactor: adds a function api

---
+++ lib/function/index.d.ts
@@ -0,0 +1,9 @@
+import { Precondition } from '../';
+/**
+ * range tests whether a number falls within a specified range.
+ */
+export declare const range: (min: number, max: number) => Precondition<number, number>;
+/**
+ * isFunction tests if a value is a function.
+ */
+export declare const isFunction: Precondition<any, Function>;
