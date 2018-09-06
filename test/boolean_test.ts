892c63dbc7eca1a108c6df8e68e5e72a9f046d6c
Lasana Murray <lmurray@quenk.com>
2018-09-06 11:52:04 -0400

    refactor: add boolean preconditions

---
+++ test/boolean_test.ts
@@ -0,0 +1,35 @@
+import * as must from 'must/register';
+import { Failure } from '../src';
+import { isBoolean, toBoolean } from '../src/boolean';
+
+describe('boolean', function() {
+
+    describe('isBoolean', function() {
+
+        it('should fail if the value specified is not a boolean', function() {
+
+            must(isBoolean(true).takeRight()).be(true);
+            must(isBoolean(false).takeRight()).be(false);
+            must(isBoolean('12').takeLeft()).be.instanceOf(Failure);
+
+        });
+
+    });
+
+    describe('toBoolean', function() {
+
+        it('should work', function() {
+
+            must(toBoolean(Date).takeRight()).be(true);
+            must(toBoolean(undefined).takeRight()).be(false);
+            must(toBoolean(null).takeRight()).be(false);
+            must(toBoolean(true).takeRight()).be(true);
+            must(toBoolean(false).takeRight()).be(false);
+
+        });
+
+    });
+
+});
+
+
