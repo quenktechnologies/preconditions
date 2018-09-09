03a847e4882264b7c1bf7fa45306524e07594675
Lasana Murray <lmurray@quenk.com>
2018-09-08 21:30:59 -0400

    chore: update Failure docs

--- src/failure.ts
+++ src/failure.ts
@@ -1,13 +1,16 @@
 import * as polate from '@quenk/polate';
-import { merge } from '@quenk/noni/lib/data/record';
+import { Record, merge } from '@quenk/noni/lib/data/record';
 
 /**
  * Explanation of what went wrong with a Precondition.
+ *
+ * This can be a string for a single precondition or a Record
+ * when multiple conditions have failed.
  */
 export type Explanation
-    = string
-    | object
-    ;
+  = string
+  | Record<string>
+  ;
 
 /**
  * Context of a failure.
@@ -16,7 +19,7 @@ export type Explanation
  */
 export interface Context {
 
-    [key: string]: any
+  [key: string]: any
 
 }
 
@@ -25,41 +28,55 @@ export interface Context {
  */
 export interface ErrorTemplates {
 
-    [key: string]: string
+  [key: string]: string
 
 }
 
 
 /**
- * Failure indicates a failure to satisfy a precondition.
+ * Failure is the class used to represent a failed precondition.
  */
 export class Failure<A> {
 
-    constructor(
-        public message: string,
-        public value?: A,
-        public context: Context = {}) { }
+  constructor(
+    public message: string,
+    public value?: A,
+    public context: Context = {}) { }
 
-    explain(templates: ErrorTemplates = {}, c: Context = {}): Explanation {
+  /**
+   * explain converts a Failure into an explanation.
+   *
+   * Explanations can be expanded by providing a hash of ErrorTemplates
+   * and a Context.
+   *
+   * This function looks up the appropriate template to use and expand in
+   * the following order:
+   * 1. `${context.$key}.${this.message}`
+   * 2. `${key}`
+   * 3. `${this.message}`
+   *
+   * If all these fail the message value is used.
+   */
+  explain(templates: ErrorTemplates = {}, context: Context = {}): Explanation {
 
-        let combined = (typeof c['$key'] === 'string') ?
-            `${c.$key}.${this.message}` :
-            this.message;
-        let key = c.$key;
-        let $value = this.value;
+    let combined = (typeof context['$key'] === 'string') ?
+      `${context.$key}.${this.message}` :
+      this.message;
+    let key = context.$key;
+    let $value = this.value;
 
-        //@todo: fix stairway to hell
-        return polate.polate(
-            ((templates[combined]) ?
-                templates[combined] :
-                (templates[<string>key]) ?
-                    templates[<string>key] :
-                    (templates[this.message.split('.')[0]]) ?
-                        templates[this.message.split('.')[0]] :
-                        (templates[this.message]) ?
-                            templates[this.message] :
-                            this.message), merge(merge(this.context, c), { $value }));
+    //@todo: fix stairway to hell
+    return polate.polate(
+      ((templates[combined]) ?
+        templates[combined] :
+        (templates[<string>key]) ?
+        templates[<string>key] :
+        (templates[this.message.split('.')[0]]) ?
+        templates[this.message.split('.')[0]] :
+        (templates[this.message]) ?
+        templates[this.message] :
+        this.message), merge(merge(this.context, context), { $value }));
 
-    }
+  }
 
 }
